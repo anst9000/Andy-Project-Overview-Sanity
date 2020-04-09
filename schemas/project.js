6d600f57cd3bd0d097bf6591a6434b4eb14ab700
Anders Stromberg <coltlaboy@yahoo.se>
2020-04-09 15:36:28 +0200

    first commit

---
+++ schemas/project.js
@@ -0,0 +1,60 @@
+export default {
+  name: 'project',
+  title: 'Project',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string'
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96
+      }
+    },
+    {
+      name: 'link',
+      title: 'Project URL',
+      type: 'url'
+    },
+    {
+      name: 'image',
+      title: 'Project Screenshot',
+      type: 'image',
+      options: {
+        hotspot: true
+      }
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'text'
+    },
+    {
+      name: 'author',
+      title: 'Author',
+      type: 'reference',
+      to: { type: 'author' }
+    }
+  ],
+
+  preview: {
+    select: {
+      title: 'title',
+      author: 'author.name',
+      media: 'image'
+    },
+    prepare(selection) {
+      const { author } = selection;
+      return {
+        ...selection,
+        subtitle: author && `${author} is a genius`
+      };
+    }
+  }
+};
